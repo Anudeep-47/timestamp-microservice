31df970142115bc12473114c5e8b9b243a79d408
Anudeep Pala <anudeep479@gmail.com>
2022-02-12 13:05:14 -0500

    Initial commit

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({
+  optionsSuccessStatus: 200
+})); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,14 +21,36 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({
+    greeting: 'hello API'
+  });
 });
 
 
+app.get("/api/:date(.{0,})", function (req, res) {
+  try {
+    let d = req.params.date;
+    if (d.length === 0) {
+      d = new Date();
+    } else {
+      if (!isNaN(+d)) d = parseInt(d);
+      d = new Date(d);
+      if (d instanceof Date && isNaN(d)) throw 'Invalid Date';
+    }
+    res.json({
+      'unix': d.getTime(),
+      'utc': d.toUTCString()
+    });
+  } catch (error) {
+    res.json({
+      error
+    });
+  }
+});
+
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
